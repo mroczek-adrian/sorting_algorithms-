959a257de1a995e7b9ae0a16b5cd7f87f3d53319
mroczek-adrian <adrian.mro1@o2.pl>
2020-09-07 11:34:53 +0200

    first animation

--- public/js/algorithms-javascript.js
+++ public/js/algorithms-javascript.js
@@ -5,14 +5,40 @@ const ctx = cvs.getContext("2d");
 // GAME VARS AND CONSTS
 let frames = 0;
 
-//LOAD SPRITE IMAGE
-//const sprite = new Image();
+
+//IMAGE OF THE BIRD
 const sprite=document.getElementById("image");
+//IMAGE OF THE NUMBERS
 const numbers=document.getElementById("numbers");
 //ctx.font = "30px Arial";
 
-//sprite.src = 'js/img/sprite.png';
-//var html = '<img class="imagesData" src="' + img + '" alt="' + alt + '">';
+
+//GAME STATE
+const state = {
+    current : 0 ,
+    getReady : 0,
+    game : 1,
+    over : 2
+}
+
+
+//CONTROL THE GAME
+cvs.addEventListener("click",function(evt){
+   switch(state.current){
+       case state.getReady:
+           state.current = state.game;
+           break;
+       case state.game:
+
+           //HERE I WILL WRITE THE ALGORITHM
+         //  bird.flap();
+           nr1.bubbleSort();
+           break;
+       case state.over:
+           state.current = state.getReady;
+           break;
+   }
+});
 
 
 //BACKGROUD
@@ -32,6 +58,17 @@ const bg = {
 
 //NUMBERS
 const nr1 = {
+    animation : [
+        {sX : 24, sY : 24 ,
+            dY : 210, dX : 20},
+        {sX : 24, sY : 24 ,
+            dY : 220, dX : 20},
+        {sX : 24, sY : 24 ,
+            dY : 230, dX : 20},
+        {sX : 24, sY : 24 ,
+            dY : 240, dX : 20}
+    ],
+
     sX : 24,
     sY : 24,
     sWidth : 168 - 24,
@@ -40,9 +77,32 @@ const nr1 = {
     dX : 20,
     dWidth : 100 - 24,
     dHeight :100 - 24 ,
+
+    //frame can change the image of the images
+    frame : 0,
+
+
     draw : function(){
-        ctx.drawImage(numbers,this.sX,this.sY,this.sWidth,this.sHeight,this.dX,this.dY,
+        let nr1 = this.animation[this.frame];
+        ctx.drawImage(numbers,nr1.sX,nr1.sY,this.sWidth,this.sHeight,nr1.dX,nr1.dY,
             this.dWidth,this.dHeight);
+    },
+
+    bubbleSort : function(){
+
+    },
+
+
+    update: function(){
+        //IF THE GAME STATE IS GET READY STATE, THE BIRD MUST FLAP SLOWLY
+        //this.period = state.current == state.getReady ? 10 : 5;
+
+        //SPEED DEPENDS ON THE NUMBER OF PERIOD
+        this.period = state.current == state.getReady ? 0 : 55;
+        //WE INCREMENT THE FRAME BY 1, EACH PERIOD
+        this.frame += frames % this.period == 0 ? 1 : 0;
+        //FRAME GOES FROM 0 TO 4, THEN AGAIN TO 0
+        this.frame = this.frame % this.animation.length;
     }
 }
 const nr2 = {
@@ -101,6 +161,41 @@ const nr9 = {
             this.dWidth,this.dHeight);
     }
 }
+
+
+//GET READY MESSAGE
+const getReady = {
+    sX: 0,
+    sY : 228,
+    w : 173,
+    h : 152,
+    x : cvs.width/2 - 173/2,
+    y : 80,
+
+    draw : function(){
+        if(state.current == state.getReady){
+            ctx.drawImage(sprite,this.sX,this.sY,this.w,this.h,this.x,this.y,
+                this.w,this.h);
+        }
+    }
+}
+const gameOver = {
+    sX : 24,
+    sY : 215,
+    sWidth : 168 - 24,
+    sHeight : 168 - 24,
+    dY : 200,
+    dX : 260,
+    dWidth : 100 - 24,
+    dHeight :100 - 24 ,
+    draw : function(){
+        if(state.current == state.over){
+            ctx.drawImage(numbers,this.sX,this.sY,this.sWidth,this.sHeight,this.dX,this.dY,
+                this.dWidth,this.dHeight);
+        }
+    }
+}
+
 //DRAW
 function draw(){
     //background
@@ -112,15 +207,21 @@ function draw(){
     ctx.strokeText("Bubble Sort!",10,50);
 
     //draw function
-    //bg.draw();
+    bg.draw();
+
+
     nr1.draw();
     nr2.draw();
     nr3.draw();
     nr4.draw();
     nr9.draw();
+
+    getReady.draw();
+    gameOver.draw();
 }
 //UPDATE
 function update(){
+    nr1.update();
 
 
 }
