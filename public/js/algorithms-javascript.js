4bf637ff37facdc884f46b1b912ec3b3c84d0545
mroczek-adrian <adrian.mro1@o2.pl>
2020-09-09 12:02:52 +0200

    analyse of bubble sort, create the simulation of bubble sort, new image of 'get ready' state

--- public/js/algorithms-javascript.js
+++ public/js/algorithms-javascript.js
@@ -4,13 +4,19 @@ const ctx = cvs.getContext("2d");
 
 // GAME VARS AND CONSTS
 let frames = 0;
+let globalSpeed = 0.25;
 
 
 //IMAGE OF THE BIRD
 const sprite=document.getElementById("image");
-//IMAGE OF THE NUMBERS
+//IMAGE OF NUMBERS
 const numbers=document.getElementById("numbers");
-//ctx.font = "30px Arial";
+//IMAGE OF THE BLUE NUMBERS
+const blue_numbers=document.getElementById("blue_numbers");
+//IMAGE OF THE COMPARISON 2 NUMBER
+const compare2numbers=document.getElementById("compare2numbers");
+//IMAGE OF THE GET READY STATE
+const click2start=document.getElementById("click2start");
 
 
 //GAME STATE
@@ -27,6 +33,7 @@ cvs.addEventListener("click",function(evt){
    switch(state.current){
        case state.getReady:
            state.current = state.game;
+           frames = 0;
            break;
        case state.game:
 
@@ -55,19 +62,98 @@ const bg = {
             this.w,this.h);
     }
 }
+//COMPARE 2 NUMBERS
+const c2n = {
+
+    sX : 328,
+    sY : 200,
+    sWidth : 510-328,
+    sHeight : 300-200,
+    dY : 280,
+    dX : 50,
+    dWidth : 100,
+    dHeight :50  ,
+    draw : function(){
+
+        ctx.drawImage(compare2numbers,this.sX,this.sY,this.sWidth,this.sHeight,this.dX,this.dY,
+            this.dWidth,this.dHeight);
+    },
+
+
+    update: function(){
+        //IF THE GAME STATE IS GET READY STATE, THE BIRD MUST FLAP SLOWLY
+        //this.period = state.current == state.getReady ? 10 : 5;
+
+        //SPEED DEPENDS ON THE NUMBER OF PERIOD
+        this.period = state.current == state.getReady ? 0 : 55;
+        //WE INCREMENT THE FRAME BY 1, EACH PERIOD
+        this.frame += frames % this.period == 0 ? 1 : 0;
+        //FRAME GOES FROM 0 TO 4, THEN AGAIN TO 0
+        //this.frame = this.frame % this.animation.length;
+
+
+        if(state.current == state.getReady){
+
+        }else{
+            // this.speed += this.gravity;
+            //COMPARISON VALUE OF NUMBERS-1st run
+            if(600 > frames && frames > 250){
+                this.dX +=globalSpeed;
+            }
+            if(1200 > frames && frames > 850){
+                this.dX +=globalSpeed;
+            }
+            if(1800 > frames && frames > 1450){
+                this.dX +=globalSpeed;
+            }
+            //COMPARISON VALUE OF NUMBERS-2nd run
+            if( frames == 2050){
+                this.dX =50;
+            }
+            if(2650 > frames && frames > 2300){
+                this.dX +=globalSpeed;
+            }
+            if(3250 > frames && frames > 2900){
+                this.dX +=globalSpeed;
+            }
+            //COMPARISON VALUE OF NUMBERS-3rd run
+            if( frames == 3500){
+                this.dX =50;
+            }
+            if(4000 > frames && frames > 3750){
+                this.dX +=globalSpeed;
+            }
+            //COMPARISON VALUE OF NUMBERS-4th run
+            if(frames == 4250){
+                this.dX =50;
+            }
+        }
+
+
+    }
+}
+
+
 
 //NUMBERS
 const nr1 = {
-    animation : [
-        {sX : 24, sY : 24 ,
-            dY : 210, dX : 20},
-        {sX : 24, sY : 24 ,
-            dY : 220, dX : 20},
-        {sX : 24, sY : 24 ,
-            dY : 230, dX : 20},
-        {sX : 24, sY : 24 ,
-            dY : 240, dX : 20}
-    ],
+    //1/3 CODE WHICH IS IMPORTANT FOR ANIMATION
+    // animation : [
+    //     {sX : 24, sY : 24 ,
+    //         dY : 210, dX : 20},
+    //     {sX : 24, sY : 24 ,
+    //         dY : 200, dX : 20},
+    //     {sX : 24, sY : 24 ,
+    //         dY : 190, dX : 20},
+    //     {sX : 24, sY : 24 ,
+    //         dY : 180, dX : 20},
+    //     {sX : 24, sY : 24 ,
+    //         dY : 190, dX : 20},
+    //     {sX : 24, sY : 24 ,
+    //         dY : 200, dX : 20},
+    //     {sX : 24, sY : 24 ,
+    //         dY : 210, dX : 20}
+    // ],
 
     sX : 24,
     sY : 24,
@@ -83,7 +169,8 @@ const nr1 = {
 
 
     draw : function(){
-        let nr1 = this.animation[this.frame];
+        //2/3 CODE WHICH IS IMPORTANT FOR ANIMATION
+        //let nr1 = this.animation[this.frame];
         ctx.drawImage(numbers,nr1.sX,nr1.sY,this.sWidth,this.sHeight,nr1.dX,nr1.dY,
             this.dWidth,this.dHeight);
     },
@@ -102,10 +189,18 @@ const nr1 = {
         //WE INCREMENT THE FRAME BY 1, EACH PERIOD
         this.frame += frames % this.period == 0 ? 1 : 0;
         //FRAME GOES FROM 0 TO 4, THEN AGAIN TO 0
-        this.frame = this.frame % this.animation.length;
+        //3/3 CODE WHICH IS IMPORTANT FOR ANIMATION-4 STATES
+        //this.frame = this.frame % this.animation.length;
     }
 }
 const nr2 = {
+    // animation : [
+    //     { sX : 230,sY : 24},
+    //     {sX : 230,sY : 24},
+    //     {sX : 230,sY : 24},
+    //     {sX : 230,sY : 24}
+    // ],
+    //green number
     sX : 230,
     sY : 24,
     sWidth : 168 - 24,
@@ -114,12 +209,90 @@ const nr2 = {
     dX : 180,
     dWidth : 100 - 24,
     dHeight :100 - 24 ,
+    //blue number
+    bsX : 162,
+    bsY : 27,
+    bsWidth : 84 - 35,
+    bsHeight : 81 - 27,
+    bdY : 215,
+    bdX : 195,
+    bdWidth : 100 - 54,
+    bdHeight :100 - 54 ,
+
+    //frame can change the image of the images
+    frame : 0,
+
+    //temporary variable
+    gravity : 0.25,
+    jump : 4.6,
+    speed : 0.25,
+
     draw : function(){
-        ctx.drawImage(numbers,this.sX,this.sY,this.sWidth,this.sHeight,this.dX,this.dY,
-            this.dWidth,this.dHeight);
+        //let nr2 = this.animation[this.frame];
+        if(this.frame % 2==0){
+            ctx.drawImage(numbers,nr2.sX,nr2.sY,this.sWidth,this.sHeight,this.dX,this.dY,
+                this.dWidth,this.dHeight);
+        }else{
+            ctx.drawImage(blue_numbers,this.bsX,this.bsY,this.bsWidth,this.bsHeight,this.bdX,this.bdY,
+                this.bdWidth,this.bdHeight);
+        }
+
+
+    },
+    bubbleSort : function(){
+
+    },
+
+
+    update: function(){
+        //IF THE GAME STATE IS GET READY STATE, THE BIRD MUST FLAP SLOWLY
+        //this.period = state.current == state.getReady ? 10 : 5;
+
+        //SPEED DEPENDS ON THE NUMBER OF PERIOD
+        this.period = state.current == state.getReady ? 0 : 55;
+        //WE INCREMENT THE FRAME BY 1, EACH PERIOD
+        this.frame += frames % this.period == 0 ? 1 : 0;
+        //FRAME GOES FROM 0 TO 4, THEN AGAIN TO 0
+        //this.frame = this.frame % this.animation.length;
+
+
+        if(state.current == state.getReady){
+
+        }else{
+           // this.speed += this.gravity;
+            //50ms DELAY BEFORE MOVING THE NUMBER
+            if(950 > frames && frames > 650){
+                this.dX -=globalSpeed;
+                this.bdX -=globalSpeed;
+            }
+            //THE END OF SIMULATION
+            if( frames == 4999){
+                this.dX =180;
+                this.bdX =195;
+            }
+        }
+
+
     }
 }
 const nr3 = {
+    // animation : [
+    //     { sX : 430,sY : 24,
+    //         dY : 210, dX : 100},
+    //     {sX : 430,sY : 24,
+    //         dY : 200, dX : 100},
+    //     {sX : 430,sY : 24,
+    //         dY : 190, dX : 100},
+    //     {sX : 430,sY : 24,
+    //         dY : 180, dX : 100},
+    //     {sX : 430,sY : 24,
+    //         dY : 190, dX : 100},
+    //     {sX : 430,sY : 24,
+    //         dY : 200, dX : 100},
+    //     {sX : 430,sY : 24,
+    //         dY : 210, dX : 100}
+    // ],
+
     sX : 430,
     sY : 24,
     sWidth : 168 - 24,
@@ -128,9 +301,55 @@ const nr3 = {
     dX : 100,
     dWidth : 100 - 24,
     dHeight :100 - 24 ,
+
+
+
+    //frame can change the image of the images
+    frame : 0,
+
+
     draw : function(){
-        ctx.drawImage(numbers,this.sX,this.sY,this.sWidth,this.sHeight,this.dX,this.dY,
+        //let nr3 = this.animation[this.frame];
+        ctx.drawImage(numbers,nr3.sX,nr3.sY,this.sWidth,this.sHeight,nr3.dX,nr3.dY,
             this.dWidth,this.dHeight);
+    },
+    bubbleSort : function(){
+
+    },
+
+
+
+
+
+    update: function(){
+        //IF THE GAME STATE IS GET READY STATE, THE BIRD MUST FLAP SLOWLY
+        //this.period = state.current == state.getReady ? 10 : 5;
+
+        //SPEED DEPENDS ON THE NUMBER OF PERIOD
+        this.period = state.current == state.getReady ? 0 : 55;
+        //WE INCREMENT THE FRAME BY 1, EACH PERIOD
+        this.frame += frames % this.period == 0 ? 1 : 0;
+        //FRAME GOES FROM 0 TO 4, THEN AGAIN TO 0
+        //this.frame = this.frame % this.animation.length;
+
+
+
+
+        if(state.current == state.getReady){
+        }else{
+            // this.speed += this.gravity;
+            //50ms DELAY BEFORE MOVING THE NUMBER
+            if(950 > frames && frames > 650){
+                this.dX +=globalSpeed;
+
+            }
+            //THE END OF SIMULATION
+            if( frames == 4999){
+                this.dX =100;
+
+            }
+        }
+
     }
 }
 const nr4 = {
@@ -162,20 +381,81 @@ const nr9 = {
     }
 }
 
+const line = {
+    sX : 115,
+    sY : 47,
+    sWidth : 5,
+    sHeight : 60,
+    dY : 150,
+    dX : 450,
+    dWidth : 10,
+    dHeight :200  ,
+    draw : function(){
+        ctx.drawImage(numbers,this.sX,this.sY,this.sWidth,this.sHeight,this.dX,this.dY,
+            this.dWidth,this.dHeight);
+    },
+
+
+    //frame can change the image of the images
+    frame : 0,
+
+    update: function(){
+        //IF THE GAME STATE IS GET READY STATE, THE BIRD MUST FLAP SLOWLY
+        //this.period = state.current == state.getReady ? 10 : 5;
+
+        //SPEED DEPENDS ON THE NUMBER OF PERIOD
+        this.period = state.current == state.getReady ? 0 : 55;
+        //WE INCREMENT THE FRAME BY 1, EACH PERIOD
+        this.frame += frames % this.period == 0 ? 1 : 0;
+        //FRAME GOES FROM 0 TO 4, THEN AGAIN TO 0
+        //this.frame = this.frame % this.animation.length;
+
+
+        if(state.current == state.getReady){
+
+        }else{
+            // this.speed += this.gravity;
+            //50ms DELAY BEFORE MOVING THE NUMBER
+            if(2500 > frames && frames > 2050){
+                this.dX -=globalSpeed;
+
+            }
+            if(3850 > frames && frames > 3500){
+                this.dX -=globalSpeed;
+
+            }
+            if(4600 > frames && frames > 4250){
+                this.dX -=globalSpeed;
+
+            }
+
+            //THE END OF SIMULATION
+            if( frames == 4999){
+                this.dX =450;
+
+            }
+        }
+
+
+    }
+}
+
 
 //GET READY MESSAGE
 const getReady = {
-    sX: 0,
-    sY : 228,
-    w : 173,
-    h : 152,
-    x : cvs.width/2 - 173/2,
-    y : 80,
+    sX : 390,
+    sY : 230,
+    sWidth : 760 - 390,
+    sHeight : 380 - 230,
+    dY : 160,
+    dX : 220,
+    dWidth : 200 - 24,
+    dHeight :100 - 24 ,
 
     draw : function(){
         if(state.current == state.getReady){
-            ctx.drawImage(sprite,this.sX,this.sY,this.w,this.h,this.x,this.y,
-                this.w,this.h);
+            ctx.drawImage(click2start,this.sX,this.sY,this.sWidth,this.sHeight,this.dX,this.dY,
+                this.dWidth,this.dHeight);
         }
     }
 }
@@ -208,20 +488,27 @@ function draw(){
 
     //draw function
     bg.draw();
-
+    c2n.draw();
 
     nr1.draw();
     nr2.draw();
     nr3.draw();
     nr4.draw();
     nr9.draw();
-
+    line.draw();
     getReady.draw();
     gameOver.draw();
 }
 //UPDATE
 function update(){
     nr1.update();
+    nr2.update();
+    nr3.update();
+    c2n.update();
+    line.update();
+    if(frames == 5000){
+        state.current = state.getReady;
+    };
 
 
 }
